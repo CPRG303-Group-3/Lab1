9e435ce72702609abdfaa6ef0b85c1fe0678e21d
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-05-20 17:20:12 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+  let listOfObjects = [
+    {
+      name: "Chioma Eme",
+      github_repo: "chiomaeme",
+      group_no: "3"
+    },
+    {
+      name: "Melbourne Marsden",
+      github_repo: "mlbrnm",
+      group_no: "3"
+    },
+    {
+      name: "Harsimarpreet Singh Sahota",
+      github_repo: "johndoe",
+      group_no: "3"
+    }
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View
+        style={styles.list}>
+        {listOfObjects.map((item) => (
+          <Text
+            key={item.github_repo}>
+            {item.name}
+          </Text>
+        ))}
+      </View>
+      <Pressable
+        onPress={() => alert("I am done with the lab")}>
+        <Text
+          style={styles.button}>
+          Hello there!
+        </Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +50,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    color: 'red'
+  },
+  list: {
+    padding: 10,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderRadius: 10,
+    marginBottom: 15
+  }
 });
